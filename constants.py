face755bfb273fcaf89e9b427a48cdf382b806f3
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-08-13 13:18:38 +0530

    Create constants.py
    This file contains constants for the main code

---
+++ constants.py
@@ -0,0 +1,5 @@
+# constants
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
+TOTAL_PAIRS = len(MAJOR_COLORS) * len(MINOR_COLORS)
