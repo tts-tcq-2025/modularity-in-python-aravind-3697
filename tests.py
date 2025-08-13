23619af3b80c5b78b5817b9017d2025a0c987478
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-08-13 13:30:33 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 # tests
-from .mapping import get_color_from_pair_number, get_pair_number_from_color
+from mapping import get_color_from_pair_number, get_pair_number_from_color
 
 def test_number_to_pair(pair_number, expected_major_color, expected_minor_color):
     major_color, minor_color = get_color_from_pair_number(pair_number)
