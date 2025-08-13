d307072db2b056e5af876982526444eed8045539
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-08-13 15:04:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # main
 from tests import run_all_tests
+from printer import generate_reference_manual
 
 if __name__ == "__main__":
     run_all_tests()
+    print("\nReference Manual:\n")
+    print(generate_reference_manual())
